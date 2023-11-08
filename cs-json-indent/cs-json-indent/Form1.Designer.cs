3fca648ac4acf33c9d49c792d193656e1b330b70
devnotepage <devnotepage@gmail.com>
2023-11-08 21:49:09 +0900

    Update

--- cs-json-indent/cs-json-indent/Form1.Designer.cs
+++ cs-json-indent/cs-json-indent/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace cs_json_indent
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            splitContainer1 = new SplitContainer();
+            textBoxInput = new TextBox();
+            textBoxOutput = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(textBoxInput);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(textBoxOutput);
+            splitContainer1.Size = new Size(1405, 765);
+            splitContainer1.SplitterDistance = 468;
+            splitContainer1.TabIndex = 0;
+            // 
+            // textBoxInput
+            // 
+            textBoxInput.Dock = DockStyle.Fill;
+            textBoxInput.Location = new Point(0, 0);
+            textBoxInput.Multiline = true;
+            textBoxInput.Name = "textBoxInput";
+            textBoxInput.Size = new Size(468, 765);
+            textBoxInput.TabIndex = 0;
+            textBoxInput.Text = "input";
+            textBoxInput.TextChanged += textBoxInput_TextChanged;
+            // 
+            // textBoxOutput
+            // 
+            textBoxOutput.Dock = DockStyle.Fill;
+            textBoxOutput.Location = new Point(0, 0);
+            textBoxOutput.Multiline = true;
+            textBoxOutput.Name = "textBoxOutput";
+            textBoxOutput.Size = new Size(933, 765);
+            textBoxOutput.TabIndex = 0;
+            textBoxOutput.Text = "output";
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1405, 765);
+            Controls.Add(splitContainer1);
+            Name = "FormMain";
+            Text = "JSONインデント整形";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private SplitContainer splitContainer1;
+        private TextBox textBoxInput;
+        private TextBox textBoxOutput;
     }
 }
--- cs-json-indent/cs-json-indent/Form1.cs
+++ cs-json-indent/cs-json-indent/Form1.cs
@@ -1,10 +1,49 @@
+using System.Text.Json;
+using System.Xml;
+
 namespace cs_json_indent
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void textBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            textBoxOutput.Text = ConvJson(textBoxInput.Text);
+        }
+
+        /// <summary>
+        /// JSON文字列のインデントを見やすく整形します。
+        /// </summary>
+        /// <param name="intput">整形前のJSON文字列</param>
+        /// <returns>整形後のJSON文字列</returns>
+        private static string ConvJson(string intputJson)
+        {
+            try
+            {
+                // JSON文字列をオブジェクトに変換
+                var obj = JsonSerializer.Deserialize<object>(intputJson);
+
+                // インデントのオプションを設定
+                var options = new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                };
+
+                // オブジェクトをインデント付きのJSON文字列に変換
+                string indentedJson = JsonSerializer.Serialize(obj, options);
+
+                // JSON文字列を返却
+                return indentedJson;
+            }
+            catch (Exception ex)
+            {
+                // エラー表示
+                return "error!!" + Environment.NewLine + Environment.NewLine + ex.Message;
+            }
         }
     }
 }
